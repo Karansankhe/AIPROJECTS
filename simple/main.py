ce6b74410b6f94901a22a1ce392e59f7212637c0
karan hareshwar sankhe <98593148+Karansankhe@users.noreply.github.com>
2024-06-07 00:30:27 +0530

    Update main.py

---
+++ simple/main.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train, X_test = X_train / 255.0, X_test / 255.0
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+model.fit(X_train, y_train, epochs=5)
+model.evaluate(X_test, y_test)
